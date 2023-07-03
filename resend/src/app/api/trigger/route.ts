3d36953cdafea47871cef4ce0a61aa7957e6a16b
D-K-P <dkp.github@pm.me>
2023-07-03 15:12:41 +0100

    Added Trigger.dev

---
+++ resend/src/app/api/trigger/route.ts
@@ -0,0 +1,28 @@
+
+import { Job, TriggerClient, eventTrigger } from "@trigger.dev/sdk";
+import { createAppRoute } from "@trigger.dev/nextjs";
+
+const client = new TriggerClient({
+  id: "resend",
+  apiKey: process.env.TRIGGER_API_KEY,
+  apiUrl: process.env.TRIGGER_API_URL,
+});
+
+new Job(client, {
+  id: "example-job",
+  name: "Example Job",
+  version: "0.0.1",
+  trigger: eventTrigger({
+    name: "example.event",
+  }),
+  run: async (payload, io, ctx) => {
+    await io.logger.info("Hello world!", { payload });
+
+    return {
+      message: "Hello world!",
+    };
+  },
+});
+
+export const { POST, dynamic } = createAppRoute(client);
+  
