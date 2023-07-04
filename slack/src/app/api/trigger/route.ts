311f32aa432dddaffe709bb4f3660031dcd34c02
D-K-P <dkp.github@pm.me>
2023-07-04 10:46:29 +0100

    Added a Slack post message Job to the route.ts file

--- slack/src/app/api/trigger/route.ts
+++ slack/src/app/api/trigger/route.ts
@@ -1,6 +1,7 @@
-
 import { Job, TriggerClient, eventTrigger } from "@trigger.dev/sdk";
 import { createAppRoute } from "@trigger.dev/nextjs";
+import { Slack } from "@trigger.dev/slack";
+import { z } from "zod";
 
 const client = new TriggerClient({
   id: "slack",
@@ -8,21 +9,34 @@ const client = new TriggerClient({
   apiUrl: process.env.TRIGGER_API_URL,
 });
 
+const slack = new Slack({
+  // This ID should match your Slack integration ID on the 'Your connected integrations' dashboard.
+  id: "slack",
+});
+
+// This job sends a basic message to a Slack channel.
 new Job(client, {
-  id: "example-job",
-  name: "Example Job",
+  id: "post-slack-message",
+  name: "Post Slack Message",
   version: "0.0.1",
   trigger: eventTrigger({
-    name: "example.event",
+    name: "slack.test",
+    schema: z.object({
+      channel: z.string(),
+      text: z.string(),
+    }),
   }),
+  integrations: {
+    slack,
+  },
   run: async (payload, io, ctx) => {
-    await io.logger.info("Hello world!", { payload });
-
-    return {
-      message: "Hello world!",
-    };
+    const response = await io.slack.postMessage("post message", {
+      // This should be your channel ID, not the channel name.
+      // You can find this at the bottom of the channel settings page.
+      channel: payload.channel,
+      text: payload.text,
+    });
   },
 });
 
 export const { POST, dynamic } = createAppRoute(client);
-  
