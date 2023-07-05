787377adf46246d1a20fc6c4618ccd5dfedfe99c
D-K-P <dkp.github@pm.me>
2023-07-05 16:36:19 +0100

    renamed delayJob

--- delays/src/app/api/trigger/route.ts
+++ delays/src/app/api/trigger/route.ts
@@ -2,7 +2,7 @@ import { createAppRoute } from "@trigger.dev/nextjs";
 import { client } from "@/trigger";
 
 // Replace this with your own jobs
-import "@/jobs/delay-job";
+import "@/jobs/delayJob";
 
 //this route is used to send and receive data with Trigger.dev
 export const { POST, dynamic } = createAppRoute(client);
